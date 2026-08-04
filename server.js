a3559d8cf97e5b59663e9540508b8dd5416a6b85
YoussefAyman17 <youssefayman8585@gmail.com>
2026-08-05 02:45:40 +0300

    try catch on db connection

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_PASSWORD);
-mongoose.connect(DB).then((con) => console.log('DB Connection Successfully'));
+mongoose
+  .connect(DB)
+  .then((con) => console.log('DB Connection Successfully'))
+  .catch((err) => console.log(err));
 const port = process.env.port || 3000;
 const server = app.listen(port, () => {
   console.log(`listening on port: ${port}`);
