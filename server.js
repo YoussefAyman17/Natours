72bc15f363b0ab9e7db83d7e2da08175a5f663b4
YoussefAyman17 <youssefayman8585@gmail.com>
2026-08-08 04:17:16 +0300

    add configuration for vercel

--- server.js
+++ server.js
@@ -11,18 +11,6 @@ process.on('uncaughtException', (err) => {
 
   process.exit(1);
 });
-dotenv.config({ path: './config.env' });
-const app = require('./app');
-
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_PASSWORD);
-mongoose
-  .connect(DB)
-  .then((con) => console.log('DB Connection Successfully'))
-  .catch((err) => console.log(err));
-// const port = process.env.port || 3000;
-// const server = app.listen(port, () => {
-//   console.log(`listening on port: ${port}`);
-// });
 
 process.on('unhandledRejection', (err) => {
   console.log('UNHANDLED REJECTION! 💥 Shutting down...');
@@ -39,4 +27,47 @@ process.on('SIGTERM', () => {
   });
 });
 
-module.exports = app;
+dotenv.config({ path: './config.env' });
+const app = require('./app');
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_PASSWORD);
+mongoose
+  .connect(DB)
+  .then((con) => console.log('DB Connection Successfully'))
+  .catch((err) => console.log(err));
+
+const connectDB = async () => {
+  if (mongoose.connection.readyState >= 1) {
+    return;
+  }
+
+  const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DB_PASSWORD,
+  );
+
+  // Await the connection
+  await mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    maxPoolSize: 10,
+    serverSelectionTimeoutMS: 5000,
+    family: 4,
+  });
+
+  console.log('DB connection established!');
+};
+
+module.exports = async (req, res) => {
+  // Force Vercel to wait for the DB before processing the route
+  await connectDB();
+
+  // Once connected, hand the request to Express
+  return app(req, res);
+};
+// const port = process.env.port || 3000;
+// const server = app.listen(port, () => {
+//   console.log(`listening on port: ${port}`);
+// });
