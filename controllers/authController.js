b3d3e28f3e9166fd716405d05053498786976d9a
YoussefAyman17 <youssefayman8585@gmail.com>
2026-08-08 04:35:23 +0300

    add configuration for vercel

--- controllers/authController.js
+++ controllers/authController.js
@@ -67,6 +67,10 @@ exports.logout = (req, res) => {
   res.cookie('jwt', 'loggedout', {
     expires: new Date(Date.now() + 10 * 1000),
     httpOnly: true,
+    secure:
+      req.secure ||
+      req.headers['x-forwarded-proto'] === 'https' ||
+      process.env.NODE_ENV === 'production',
   });
   res.status(200).json({ status: 'success' });
 };
