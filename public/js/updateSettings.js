4e7c777b3a75a9f929ad8db911b4123a9d5a76e4
YoussefAyman17 <youssefayman8585@gmail.com>
2026-08-08 21:49:59 +0300

    add configuration for vercel

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -5,15 +5,15 @@ export const updateSettings = async (data, type) => {
     console.log(data);
     const url =
       type === 'password'
-        ? 'http://127.0.0.1:3000/api/v1/users/updateMyPassword'
-        : 'http://127.0.0.1:3000/api/v1/users/updateMe';
+        ? '/api/v1/users/updateMyPassword'
+        : '/api/v1/users/updateMe';
 
     const res = await axios({
       method: 'PATCH',
       url,
       data,
     });
-    console.log(res);
+    // console.log(res);
 
     if (res.data.status === 'success') {
       showAlert('success', `${type.toUpperCase()} updated successfully!`);
