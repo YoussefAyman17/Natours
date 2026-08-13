5865f5d3d3cb9da3e3da7202ff199b292df56a65
YoussefAyman17 <youssefayman8585@gmail.com>
2026-08-13 17:26:16 +0300

    add configuration for stripe webhooks

--- app.js
+++ app.js
@@ -16,11 +16,15 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const viewRouter = require('./routes/viewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
+const bookingController = require('./controllers/bookingController');
 
 const app = express();
 
 app.enable('trust proxy');
 app.set('trust proxy', 1);
+
+app.use(cors());
+app.options('*', cors());
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
@@ -74,6 +78,12 @@ const limiter = rateLimit({
 });
 
 app.use('/api', limiter);
+
+app.post(
+  '/webhook-checkout',
+  express.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout,
+);
 
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,6 +1,7 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Booking = require('../models/bookingModel');
 const Tour = require('../models/tourModel');
+const User = require('../models/userModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
@@ -12,7 +13,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     payment_method_types: 'card',
 
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
-    success_url: `${req.protocol}://${req.get('host')}/`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
     client_reference_id: req.params.tourId,
     customer_email: req.user.email,
     mode: 'payment',
@@ -24,7 +25,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
           product_data: {
             name: `${tour.name} Tour`,
             description: tour.summary,
-            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+            images: [
+              `https://natours-pi-lime.vercel.app/img/tours/${tour.imageCover}`,
+            ],
           },
         },
         quantity: 1,
@@ -38,11 +41,30 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.createBookingCheckout = catchAsync(async (req, res, next) => {
-  // This is only TEMPORARY, because it's UNSECURE: everyone can make bookings without paying
-  const { tour, user, price } = req.query;
+exports.webhookCheckout = (req, res, next) => {
+  const signature = req.headers['stripe-signature'];
+  let event;
+  try {
+    event = stripe.webhooks.constructEvent(
+      req.body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET,
+    );
+  } catch (err) {
+    return res.status(400).send(`Webhook error: ${err.message}`);
+  }
+  if (event.type === 'checkout.session.completed')
+    createBookingCheckout(event.data.object);
 
-  if (!tour && !user && !price) return next();
+  res.status(200).json({ received: true });
+};
+
+exports.createBookingCheckout = catchAsync(async (session) => {
+  // This is only TEMPORARY, because it's UNSECURE: everyone can make bookings without paying
+  const tour = session.client_reference_id;
+  const user = (await User.find({ email: session.customer_email })).id;
+  const price = session.amount_total / 100;
+
   await Booking.create({ tour, user, price });
 
   res.redirect(req.originalUrl.split('?')[0]);
