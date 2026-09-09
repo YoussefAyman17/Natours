71eb003775f16678a8ad4b427d0a8fa815c69d95
YoussefAyman17 <youssefayman8585@gmail.com>
2026-09-09 20:18:56 +0300

    Make signup page

--- utils/email.js
+++ utils/email.js
@@ -13,15 +13,15 @@ module.exports = class Email {
   }
 
   newTransport() {
-    if (process.env.NODE_ENV === 'production') {
-      return Nodemailer.createTransport({
-        service: 'SendGrid',
-        auth: {
-          user: process.env.SENDGRID_USERNAME,
-          pass: process.env.SENDGRID_PASSWORD,
-        },
-      });
-    }
+    // if (process.env.NODE_ENV === 'production') {
+    //   return Nodemailer.createTransport({
+    //     service: 'SendGrid',
+    //     auth: {
+    //       user: process.env.SENDGRID_USERNAME,
+    //       pass: process.env.SENDGRID_PASSWORD,
+    //     },
+    //   });
+    // }
     const TOKEN = process.env.EMAIL_TOKEN;
     return Nodemailer.createTransport(
       MailtrapTransport({
